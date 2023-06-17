d453304d595f13778bbc5f37441424b801cfc22d
Denis Ivanchenko <sicknature24@yandex.com>
2023-06-17 14:34:41 +0300

    feat: move html elements to separated class

---
+++ rs-selectors/src/components/level/Elements.ts
@@ -0,0 +1,59 @@
+import DOMHelpers from '../utils/DOMHelpers';
+
+class Elements {
+    public levelWrapper: HTMLElement;
+
+    public navElements: HTMLElement;
+
+    public nextLevel: HTMLElement;
+
+    public prevLevel: HTMLElement;
+
+    public burger: HTMLElement;
+
+    public burgerLine: HTMLElement;
+
+    public levelNumber: HTMLElement;
+
+    public levelProgress: HTMLElement;
+
+    public levelProgressState: HTMLElement;
+
+    public levelBlock: HTMLElement;
+
+    public selectorType: HTMLElement;
+
+    public selectorTitle: HTMLElement;
+
+    public selectorSyntax: HTMLElement;
+
+    public selectorHint: HTMLElement;
+
+    public exampleTitle: HTMLElement;
+
+    public exampleCase: HTMLElement;
+
+    public checkMark: HTMLElement;
+
+    constructor() {
+        this.levelWrapper = DOMHelpers.createElement('div', ['level-wrapper']);
+        this.navElements = DOMHelpers.createElement('div', ['level__nav-elements']);
+        this.nextLevel = DOMHelpers.createElement('button', ['nav-elements__nextLevel'], '>');
+        this.prevLevel = DOMHelpers.createElement('button', ['nav-elements__prevLevel'], '<');
+        this.burger = DOMHelpers.createElement('div', ['nav-elements__burger']);
+        this.burgerLine = DOMHelpers.createElement('span', ['burger__burger-line']);
+        this.levelNumber = DOMHelpers.createElement('span', ['nav-elements__level-number']);
+        this.levelProgress = DOMHelpers.createElement('div', ['nav-elements__level-progress']);
+        this.levelProgressState = DOMHelpers.createElement('div', ['level-progress__state']);
+        this.levelBlock = DOMHelpers.createElement('div', ['nav-elements__block']);
+        this.selectorType = DOMHelpers.createElement('h3', ['nav-elements__selector-name']);
+        this.selectorTitle = DOMHelpers.createElement('h2', ['nav-elements__selector-title']);
+        this.selectorSyntax = DOMHelpers.createElement('h2', ['nav-elements__selector-syntax']);
+        this.selectorHint = DOMHelpers.createElement('div', ['nav-elements__selector-hint']);
+        this.exampleTitle = DOMHelpers.createElement('h4', ['nav-elements__example-title']);
+        this.exampleCase = DOMHelpers.createElement('div', ['nav-elements__example-case']);
+        this.checkMark = DOMHelpers.createElement('span', ['nav-elements__checkmark']);
+    }
+}
+
+export default Elements;
--- rs-selectors/src/components/level/Layout.ts
+++ rs-selectors/src/components/level/Layout.ts
@@ -1,83 +1,58 @@
-import DOMHelpers from '../utils/DOMHelpers';
 import { LevelInfo } from '../../types/Interfaces';
+import Menu from './Menu';
+import Elements from './Elements';
 
 class Layout {
     private levels: LevelInfo[];
 
     private currentLevelIndex: number;
 
-    private levelWrapper: HTMLElement = DOMHelpers.createElement('div', ['level-wrapper']);
+    private levelElements: Elements;
 
-    private navElements: HTMLElement = DOMHelpers.createElement('div', ['level__nav-elements']);
-
-    private nextLevel: HTMLElement = DOMHelpers.createElement('button', ['nav-elements__nextLevel'], '>');
-
-    private prevLevel: HTMLElement = DOMHelpers.createElement('button', ['nav-elements__prevLevel'], '<');
-
-    private burger: HTMLElement = DOMHelpers.createElement('div', ['nav-elements__burger']);
-
-    private burgerLine: HTMLElement = DOMHelpers.createElement('span', ['burger__burger-line']);
-
-    private levelNumber: HTMLElement = DOMHelpers.createElement('span', ['nav-elements__level-number']);
-
-    private levelProgress: HTMLElement = DOMHelpers.createElement('div', ['nav-elements__level-progress']);
-
-    private levelProgressState: HTMLElement = DOMHelpers.createElement('div', ['level-progress__state']);
-
-    private levelBlock: HTMLElement = DOMHelpers.createElement('div', ['nav-elements__block']);
-
-    private selectorType: HTMLElement = DOMHelpers.createElement('h3', ['nav-elements__selector-name']);
-
-    private selectorTitle: HTMLElement = DOMHelpers.createElement('h2', ['nav-elements__selector-title']);
-
-    private selectorSyntax: HTMLElement = DOMHelpers.createElement('h2', ['nav-elements__selector-syntax']);
-
-    private selectorHint: HTMLElement = DOMHelpers.createElement('div', ['nav-elements__selector-hint']);
-
-    private exampleTitle: HTMLElement = DOMHelpers.createElement('h4', ['nav-elements__example-title']);
-
-    private exampleCase: HTMLElement = DOMHelpers.createElement('div', ['nav-elements__example-case']);
-
-    private checkMark: HTMLElement = DOMHelpers.createElement('span', ['nav-elements__checkmark']);
+    private menu: Menu;
 
     constructor(levels: LevelInfo[]) {
         this.levels = levels;
         this.currentLevelIndex = 0;
+        this.levelElements = new Elements();
+        this.menu = new Menu(this.levelElements.burger);
     }
 
     private appendElements = (): void => {
-        document.body.appendChild(this.levelWrapper);
-        this.levelWrapper.appendChild(this.navElements);
-        this.levelWrapper.appendChild(this.levelProgress);
-        this.levelProgress.appendChild(this.levelProgressState);
-        this.levelWrapper.appendChild(this.levelBlock);
-        this.navElements.appendChild(this.levelNumber);
-        this.navElements.appendChild(this.checkMark);
-        this.navElements.appendChild(this.prevLevel);
-        this.navElements.appendChild(this.nextLevel);
-        this.navElements.appendChild(this.burger);
-        this.burger.appendChild(this.burgerLine);
-        this.levelBlock.appendChild(this.selectorType);
-        this.levelBlock.appendChild(this.selectorTitle);
-        this.levelBlock.appendChild(this.selectorSyntax);
-        this.levelBlock.appendChild(this.selectorHint);
-        this.levelBlock.appendChild(this.exampleTitle);
-        this.levelBlock.appendChild(this.exampleCase);
+        document.body.appendChild(this.levelElements.levelWrapper);
+        this.levelElements.levelWrapper.appendChild(this.levelElements.navElements);
+        this.levelElements.levelWrapper.appendChild(this.levelElements.levelProgress);
+        this.levelElements.levelProgress.appendChild(this.levelElements.levelProgressState);
+        this.levelElements.levelWrapper.appendChild(this.levelElements.levelBlock);
+        this.levelElements.navElements.appendChild(this.levelElements.levelNumber);
+        this.levelElements.navElements.appendChild(this.levelElements.checkMark);
+        this.levelElements.navElements.appendChild(this.levelElements.prevLevel);
+        this.levelElements.navElements.appendChild(this.levelElements.nextLevel);
+        this.levelElements.navElements.appendChild(this.levelElements.burger);
+        this.levelElements.burger.appendChild(this.levelElements.burgerLine);
+        this.levelElements.levelBlock.appendChild(this.levelElements.selectorType);
+        this.levelElements.levelBlock.appendChild(this.levelElements.selectorTitle);
+        this.levelElements.levelBlock.appendChild(this.levelElements.selectorSyntax);
+        this.levelElements.levelBlock.appendChild(this.levelElements.selectorHint);
+        this.levelElements.levelBlock.appendChild(this.levelElements.exampleTitle);
+        this.levelElements.levelBlock.appendChild(this.levelElements.exampleCase);
     };
 
     private assignEventListeners = (): void => {
-        this.nextLevel.addEventListener('click', this.handleNextLevel);
-        this.prevLevel.addEventListener('click', this.handlePrevLevel);
+        this.levelElements.nextLevel.addEventListener('click', this.handleNextLevel);
+        this.levelElements.prevLevel.addEventListener('click', this.handlePrevLevel);
     };
 
     private changeProgressState = (option: string): void => {
-        let currentWidth = parseFloat(getComputedStyle(this.levelProgressState).getPropertyValue('width'));
+        let currentWidth = parseFloat(
+            getComputedStyle(this.levelElements.levelProgressState).getPropertyValue('width')
+        );
         if (option === 'increase') {
             currentWidth += 35;
-            this.levelProgressState.style.setProperty('width', `${currentWidth}px`);
+            this.levelElements.levelProgressState.style.setProperty('width', `${currentWidth}px`);
         } else {
             currentWidth -= 35;
-            this.levelProgressState.style.setProperty('width', `${currentWidth}px`);
+            this.levelElements.levelProgressState.style.setProperty('width', `${currentWidth}px`);
         }
     };
 
@@ -98,13 +73,15 @@ class Layout {
     };
 
     private populateLevelData = (): void => {
-        this.levelNumber.innerText = `${this.levels[this.currentLevelIndex].levelIndicator} of ${this.levels.length}`;
-        this.selectorType.innerText = this.levels[this.currentLevelIndex].selectorType;
-        this.selectorTitle.innerText = this.levels[this.currentLevelIndex].selectorTitle;
-        this.selectorSyntax.innerText = this.levels[this.currentLevelIndex].selectorSyntax;
-        this.selectorHint.innerText = this.levels[this.currentLevelIndex].hint;
-        this.exampleTitle.innerText = this.levels[this.currentLevelIndex].example.title;
-        this.exampleCase.innerText = `${this.levels[this.currentLevelIndex].example.case1} \n ${
+        this.levelElements.levelNumber.innerText = `${this.levels[this.currentLevelIndex].levelIndicator} of ${
+            this.levels.length
+        }`;
+        this.levelElements.selectorType.innerText = this.levels[this.currentLevelIndex].selectorType;
+        this.levelElements.selectorTitle.innerText = this.levels[this.currentLevelIndex].selectorTitle;
+        this.levelElements.selectorSyntax.innerText = this.levels[this.currentLevelIndex].selectorSyntax;
+        this.levelElements.selectorHint.innerText = this.levels[this.currentLevelIndex].hint;
+        this.levelElements.exampleTitle.innerText = this.levels[this.currentLevelIndex].example.title;
+        this.levelElements.exampleCase.innerText = `${this.levels[this.currentLevelIndex].example.case1} \n ${
             this.levels[this.currentLevelIndex].example.case2
         }`;
     };
@@ -113,6 +90,7 @@ class Layout {
         this.appendElements();
         this.assignEventListeners();
         this.populateLevelData();
+        this.menu.init();
     };
 }
 
