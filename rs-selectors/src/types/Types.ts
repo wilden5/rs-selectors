99f595029720f0649af3ce5f6ba2b98131adecd3
Denis Ivanchenko <sicknature24@yandex.com>
2023-06-19 18:59:00 +0300

    feat: add BarProgressStateMap type with related constant

---
+++ rs-selectors/src/types/Types.ts
@@ -0,0 +1,16 @@
+export type BarProgressStateMap = {
+    [levelIndex: number]: number;
+};
+
+export const barProgressStateMap: BarProgressStateMap = {
+    0: 35,
+    1: 35 * 2,
+    2: 35 * 3,
+    3: 35 * 4,
+    4: 35 * 5,
+    5: 35 * 6,
+    6: 35 * 7,
+    7: 35 * 8,
+    8: 35 * 9,
+    9: 35 * 10,
+};
