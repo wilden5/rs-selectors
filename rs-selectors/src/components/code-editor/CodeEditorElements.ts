95f1f1cf0ae2fa77d0ad15a039fefb82e42b13fe
Denis Ivanchenko <sicknature24@yandex.com>
2023-06-22 14:47:57 +0300

    feat: add code-editor elements with styles; generateLineNumbers function

--- rs-selectors/src/components/code-editor/CodeEditorElements.ts
+++ rs-selectors/src/components/code-editor/CodeEditorElements.ts
@@ -14,6 +14,24 @@ class CodeEditorElements {
 
     public twitterLink: HTMLElement;
 
+    public editor: HTMLElement;
+
+    public editorHtmlView: HTMLElement;
+
+    public editorInput: HTMLElement;
+
+    public inputHeader: HTMLElement;
+
+    public htmlFileName: HTMLElement;
+
+    public htmlFileWindow: HTMLElement;
+
+    public htmlLineNumbers: HTMLElement;
+
+    public htmlLineMarkup: HTMLElement;
+
+    public markupCodeWrapper: HTMLElement;
+
     public constructor() {
         this.codeEditorContainer = DOMHelpers.createElement('div', ['code-editor-container']);
         this.header = DOMHelpers.createElement('div', ['header'], "Wilden's CSS Diner");
@@ -21,6 +39,15 @@ class CodeEditorElements {
         this.emailLink = DOMHelpers.createElement('a', ['socials__email']);
         this.facebookLink = DOMHelpers.createElement('a', ['socials__facebook']);
         this.twitterLink = DOMHelpers.createElement('a', ['socials__twitter']);
+        this.editor = DOMHelpers.createElement('div', ['editor']);
+        this.editorHtmlView = DOMHelpers.createElement('div', ['editor__table', 'editor__view']);
+        this.editorInput = DOMHelpers.createElement('div', ['editor__table', 'editor__input']);
+        this.inputHeader = DOMHelpers.createElement('div', ['table__input-header'], 'HTML Viewer');
+        this.htmlFileName = DOMHelpers.createElement('div', ['view__file-name'], 'table.html');
+        this.htmlFileWindow = DOMHelpers.createElement('div', ['view__file-window']);
+        this.htmlLineNumbers = DOMHelpers.createElement('div', ['table__line-numbers']);
+        this.htmlLineMarkup = DOMHelpers.createElement('div', ['view__markup']);
+        this.markupCodeWrapper = DOMHelpers.createElement('div', ['view__markup-code-wrapper'], '<div class="table">');
     }
 
     public static getInstance(): CodeEditorElements {
--- rs-selectors/src/components/code-editor/CodeEditorLayout.ts
+++ rs-selectors/src/components/code-editor/CodeEditorLayout.ts
@@ -11,16 +11,32 @@ class CodeEditorLayout {
     private appendCodeEditorElements(): void {
         document.body.appendChild(this.codeEditorElements.codeEditorContainer);
         this.codeEditorElements.codeEditorContainer.appendChild(this.codeEditorElements.header);
+        this.codeEditorElements.codeEditorContainer.appendChild(this.codeEditorElements.editor);
         this.codeEditorElements.header.appendChild(this.codeEditorElements.socials);
         this.codeEditorElements.socials.appendChild(this.codeEditorElements.emailLink);
         this.codeEditorElements.socials.appendChild(this.codeEditorElements.facebookLink);
         this.codeEditorElements.socials.appendChild(this.codeEditorElements.twitterLink);
+        this.codeEditorElements.editor.appendChild(this.codeEditorElements.editorInput);
+        this.codeEditorElements.editor.appendChild(this.codeEditorElements.editorHtmlView);
+        this.codeEditorElements.editorHtmlView.appendChild(this.codeEditorElements.inputHeader);
+        this.codeEditorElements.inputHeader.appendChild(this.codeEditorElements.htmlFileName);
+        this.codeEditorElements.editorHtmlView.appendChild(this.codeEditorElements.htmlFileWindow);
+        this.codeEditorElements.htmlFileWindow.appendChild(this.codeEditorElements.htmlLineNumbers);
+        this.codeEditorElements.htmlFileWindow.appendChild(this.codeEditorElements.htmlLineMarkup);
+        this.codeEditorElements.htmlLineMarkup.appendChild(this.codeEditorElements.markupCodeWrapper);
     }
 
     private appendImages(): void {
         DOMHelpers.appendImage(this.codeEditorElements.emailLink, '../../assets/img/email.png');
         DOMHelpers.appendImage(this.codeEditorElements.facebookLink, '../../assets/img/facebook.png');
         DOMHelpers.appendImage(this.codeEditorElements.twitterLink, '../../assets/img/twitter.png');
+    }
+
+    private generateLineNumbers(number: number, element: string): void {
+        for (let i = 1; i <= number; i += 1) {
+            DOMHelpers.getElement(element).innerHTML += `${i}<br>`;
+        }
+        DOMHelpers.getElement('.view__markup').appendChild(DOMHelpers.createElement('div', ['div'], '</div>'));
     }
 
     private assignCodeEditorEventListeners(): void {}
@@ -32,6 +48,7 @@ class CodeEditorLayout {
         this.appendImages();
         this.assignCodeEditorEventListeners();
         this.populateCodeEditorData();
+        this.generateLineNumbers(15, '.table__line-numbers');
     }
 }
 
