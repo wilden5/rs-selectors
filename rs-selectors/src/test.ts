693b2f66b646fc3ac8d0156c087d659c27d07365
Denis Ivanchenko <sicknature24@yandex.com>
2023-07-02 23:40:38 +0300

    feat: add more unit tests

--- rs-selectors/src/test.ts
+++ rs-selectors/src/test.ts
@@ -1,6 +1,8 @@
 import CodeEditorInput from './components/code-editor/CodeEditorInput';
 import DOMHelpers from './components/utils/DOMHelpers';
-import { userCorrectSelector, userIncorrectSelector } from './components/utils/LevelHelpers';
+import { syncLevelStatusCheckmark, userCorrectSelector, userIncorrectSelector } from './components/utils/LevelHelpers';
+import GAME_LEVELS from './components/app/AppLevels';
+import * as LevelHelpers from './components/utils/LevelHelpers';
 
 describe('CodeEditorInput', () => {
     let codeEditorInput: CodeEditorInput;
@@ -93,5 +95,60 @@ describe('userIncorrectSelector', () => {
         jest.runAllTimers();
 
         expect(editor.classList.contains('editor--blocked')).toBe(false);
+    });
+});
+
+describe('syncLevelStatusCheckmark', () => {
+    beforeEach(() => {
+        const mockElement = DOMHelpers.createElement('div', ['nav__checkmark']);
+        jest.spyOn(DOMHelpers, 'getElement').mockReturnValue(mockElement);
+
+        jest.spyOn(LevelHelpers, 'getCurrentLevelIndex').mockReturnValue(0);
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    it('should set the border color to "#35ff00" when the level status is true and hint is not used', () => {
+        GAME_LEVELS[0].status = true;
+        GAME_LEVELS[0].isHintUsed = false;
+
+        syncLevelStatusCheckmark();
+
+        expect(DOMHelpers.getElement('.nav__checkmark').style.borderColor).toBe('#35ff00');
+    });
+
+    it('should set the border color to "#faf102" when the level status is true and hint is used', () => {
+        GAME_LEVELS[0].status = true;
+        GAME_LEVELS[0].isHintUsed = true;
+
+        syncLevelStatusCheckmark();
+
+        expect(DOMHelpers.getElement('.nav__checkmark').style.borderColor).toBe('#faf102');
+    });
+
+    it('should set the border color to "#fff" when the level status is false and hint is not used', () => {
+        GAME_LEVELS[0].status = false;
+        GAME_LEVELS[0].isHintUsed = false;
+
+        syncLevelStatusCheckmark();
+
+        expect(DOMHelpers.getElement('.nav__checkmark').style.borderColor).toBe('#fff');
+    });
+});
+
+describe('appendChildToElement', () => {
+    let parentElement: HTMLElement;
+    let childElement: HTMLElement;
+
+    beforeEach(() => {
+        parentElement = DOMHelpers.createElement('div');
+        childElement = DOMHelpers.createElement('span');
+    });
+
+    it('should append the child element to the parent element', () => {
+        DOMHelpers.appendChildToElement(parentElement, childElement);
+        expect(parentElement.children).toContain(childElement);
     });
 });
